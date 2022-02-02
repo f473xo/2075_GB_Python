84d8ba79230ede8d5c18d2537ab044b73a6fe35a
f473 <iddqdxx666@gmail.com>
2022-02-02 21:36:29 +0500

    task_03 done

---
+++ Ushakov_Aleksandr_dz_03.py
@@ -0,0 +1,10 @@
+def transform_sring(number: int) -> str:
+    if number % 10 == 1 and number != 11:
+        print((number), 'процент')
+    elif number % 10 == 2 and number != 12 or number % 10 == 3 and number != 13 or number % 10 == 4 and number != 14:
+        print((number), 'процента')
+    else:
+        print((number), 'процентов')
+
+for n in range(1, 101):
+    print(transform_sring(n))
