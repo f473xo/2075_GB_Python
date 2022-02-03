ba71ed17c467cc76983aec459e29af590ef24ef9
f473 <iddqdxx666@gmail.com>
2022-02-03 16:21:44 +0500

    task_02 not done

--- Ushakov_Aleksandr_dz_02.py
+++ Ushakov_Aleksandr_dz_02.py
@@ -1,1 +1,20 @@
-# Виноват, не успел, только недавно с командировки, постараюсь как можно быстрее доздать.
+def sum_list_1(dataset: list) -> int:
+    indx = 0
+    my_list = []
+    while indx > 0 and indx < 1001:
+        sum = 0
+        elem = dataset[indx]
+        while elem != 0:
+            sum = sum = elem % 10
+            elem = elem // 10
+            return
+
+
+    #"""Вычисляет сумму чисел списка dataset, сумма цифр которых делится нацело на 7"""
+my_list = [i**3 for i in range(1, 1001, 2)]
+
+
+
+
+
+#print(my_list)
