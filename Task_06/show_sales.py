b0a5639bd81c5795c54a67cc88ce61fc911a0383
f473 <iddqdxx666@gmail.com>
2022-02-19 15:01:13 +0500

    done

--- Task_06/show_sales.py
+++ Task_06/show_sales.py
@@ -1,12 +1,12 @@
 from sys import argv
 
-with open('bakery.csv', 'r', encoding='utf-8') as r_sum:
+with open('bakery.csv', 'r', encoding='utf-8') as f:
     if len(argv) > 1:
         if len(argv) == 3:
-            sl = (list(i.strip() for i in r_sum))[int(argv[1]):int(argv[2])]
-            print(*sl)
+            sale = (list(i.strip() for i in f))[int(argv[1]):int(argv[2])]
+            print(*sale)
         if len(argv) == 2:
-            sl = (list(i.strip() for i in r_sum))[int(argv[1]):]
-            print(*sl)
+            sale = (list(i.strip() for i in f))[int(argv[1]):]
+            print(*sale)
     if len(argv) <= 1:
-        print(r_sum.read())
+        print(f.read())
